53f488364df08c6f46dad0795611dba8d5339ec2
Adam Cornett <adam.cornett@yourcharlotteschools.net>
2019-02-28 11:14:21 -0500

    Setup electron framework

--- SWRPGCombatTracker/Program.cs
+++ SWRPGCombatTracker/Program.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using ElectronNET.API;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 
 namespace SWRPGCombatTracker
 {
@@ -19,6 +13,7 @@ namespace SWRPGCombatTracker
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+            .UseElectron(args)
                 .UseStartup<Startup>();
     }
 }
--- SWRPGCombatTracker/Startup.cs
+++ SWRPGCombatTracker/Startup.cs
@@ -1,13 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using ElectronNET.API;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System.Threading.Tasks;
 
 namespace SWRPGCombatTracker
 {
@@ -30,7 +28,6 @@ namespace SWRPGCombatTracker
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
-
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
@@ -47,7 +44,6 @@ namespace SWRPGCombatTracker
             }
 
             app.UseStaticFiles();
-            app.UseCookiePolicy();
 
             app.UseMvc(routes =>
             {
@@ -55,6 +51,8 @@ namespace SWRPGCombatTracker
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+
+            Task.Run(async () => await Electron.WindowManager.CreateWindowAsync());
         }
     }
 }
